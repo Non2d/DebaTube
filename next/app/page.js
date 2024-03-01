30af29555653224b16ff8aa606915615519613e3
non2d <onedanijo11@gmail.com>
2024-03-01 17:13:19 +0900

    DebateFlow Created

--- next/app/page.js
+++ next/app/page.js
@@ -1,13 +1,9 @@
-import BarChart from '../components/BarChart';
-import DynamicChart from '../components/DynamicChart';
-import * as d3 from 'd3';
+import DebateFlow from '../componentsRF/DebateFlow';
 
 export default function Home() {
-  const data = [12, 36, 6, 25, 30, 10, 12]; // 例えば、このデータをビジュアライズします。
   return (
     <div>
-      <BarChart data={data} />
-      <DynamicChart m={200} n={10} k={10}/>
+      <DebateFlow />
     </div>
   );
 }
