3f1d5e908e0562c0c58a3bf4d4ebb0c96ee83c16
onedanijo11@gmail.com <onedanijo11@gmail.comy>
2024-04-28 18:44:41 +0900

    Add POIs field to Task model

--- fastapi/api/models/task.py
+++ fastapi/api/models/task.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, JSON
 from sqlalchemy.orm import relationship
 
 from db import Base
@@ -9,6 +9,7 @@ class Task(Base):
     id = Column(Integer, primary_key=True)
     motion = Column(String(1024))
     source = Column(String(1024))
+    POIs = Column(JSON)
 
     done = relationship("Done", back_populates="task", cascade="delete")
 
--- fastapi/api/schemas/task.py
+++ fastapi/api/schemas/task.py
@@ -1,9 +1,10 @@
-from typing import Optional
+from typing import Optional, List
 from pydantic import BaseModel, Field #BaseModelはFastAPIで使われるスキーマモデルクラスのベースクラス
 
 class TaskBase(BaseModel): #共通のフィールドを持つベースクラスを定義
     motion: Optional[str] = Field(None, example="This House Would Ban Tabacco.")
     source: Optional[str] = Field(None, example="WSDC_2019_R1_sKJoTL0Amk0.mp3")
+    POIs: List[int] = Field(None, example="[1, 2, 3]")
 
 class TaskCreate(TaskBase):
     pass
