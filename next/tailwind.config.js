33b2605ba538e5c8ad71f5d085b485810f6548e6
onedanijo11@gmail.com <onedanijo11@gmail.comy>
2024-10-26 21:58:14 +0900

    Disable purge in tailwind.config.js

--- next/tailwind.config.js
+++ next/tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   purge: {
-    enabled: true,
-    content: [
-      "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-      "./components/**/*.{js,ts,jsx,tsx,mdx}",
-      "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    ]
+    enabled: false,
+    // content: [
+    //   "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    //   "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    //   "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    // ]
   },
   theme: {
     extend: {
